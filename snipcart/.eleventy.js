98473767bdfa9a3e86a8e1966ebceedb2d9214f7
Begerad, Stefan <stefan@begerad.de>
2021-10-03 07:55:48 -0400

    feat(snipcart): added _data

--- snipcart/.eleventy.js
+++ snipcart/.eleventy.js
@@ -1,4 +1,8 @@
+//eleventy config file
 module.exports = function(eleventyConfig) {
+    //the addPassthroughCopy function takes globs of any sort
+    //and can be used multiple times
     eleventyConfig.addPassthroughCopy("style");
+    //get eleventy to notice changes in the stylce dir
     eleventyConfig.addWatchTarget("style");
 };
