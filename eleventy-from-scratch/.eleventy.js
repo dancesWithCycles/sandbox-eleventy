8469cb443147b054bab64d2847aa5dfdf84676ee
Begerad, Stefan <stefan@begerad.de>
2021-10-06 10:28:30 -0400

    feat(eleventy-from-scratch): added docu to Eleventy config file

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -1,3 +1,6 @@
+//export a function that returns some settings saying
+//look in the src folder for content, templates and other source code and
+//use dist as output folder
 module.exports = config => {
   return {
     dir: {
