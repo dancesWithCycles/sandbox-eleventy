70e26946d9d2b8b9a6c689ed6f1bf8c265c4e0f0
Begerad, Stefan <stefan@begerad.de>
2021-10-06 19:47:35 -0400

    feat(eleventy-from-scratch): added to named collections to Eleventy config file

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -3,6 +3,25 @@ module.exports = config => {
     // pass src folders through to the output folder
     config.addPassthroughCopy('./src/images/');
 
+    //add collection named work
+    config.addCollection('work', collection => {
+
+	//return work items, sorted by displayOrder
+	return collection
+	    .getFilteredByGlob('./src/work/*.md')
+	    .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+    });
+
+    //add collection named featuredWork
+    config.addCollection('featuredWork', collection => {
+
+	//return work items, sorted by dispalayOder, with featured equals to true
+	return collection
+	    .getFilteredByGlob('./src/work/*.md')
+	    .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+	    .filter(x => x.data.featured);
+    });
+
     return {
 
 	//tell 11ty that markdown, data and HTML files are processed by Nunjucks.js
