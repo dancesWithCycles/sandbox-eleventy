e9fd4a6281c050cdc069d4b265482b078235a1e5
Begerad, Stefan <stefan@begerad.de>
2021-10-06 10:56:22 -0400

    feat(eleventy-from-scratch): told 11ty that md, HTML and data files are processed by Nunjuck.js

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -1,11 +1,14 @@
-//export a function that returns some settings saying
-//look in the src folder for content, templates and other source code and
-//use dist as output folder
+//export a function that returns some settings
 module.exports = config => {
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist'
-    }
-  };
+return {
+  //tell 11ty that markdown, data and HTML files are processed by Nunjucks.js
+  markdownTemplateEngine: 'njk',
+  dataTemplateEngine: 'njk',
+  htmlTemplateEngine: 'njk',
+  //tel 11ty to look in the src folder for content, templates and other source code and
+  //use dist as output folder
+  dir: {
+    input: 'src',
+    output: 'dist'
+  }
 };
