e9e662ddb05ec4f465b0aafcf060513651f860b0
Begerad, Stefan <stefan@begerad.de>
2021-10-06 13:03:21 -0400

    feat(eleventy-from-scratch): fixed .js .json errors

--- eleventy-from-scratch/.eleventy.js
+++ eleventy-from-scratch/.eleventy.js
@@ -1,14 +1,18 @@
 //export a function that returns some settings
 module.exports = config => {
-return {
-  //tell 11ty that markdown, data and HTML files are processed by Nunjucks.js
-  markdownTemplateEngine: 'njk',
-  dataTemplateEngine: 'njk',
-  htmlTemplateEngine: 'njk',
-  //tel 11ty to look in the src folder for content, templates and other source code and
-  //use dist as output folder
-  dir: {
-    input: 'src',
-    output: 'dist'
-  }
+
+    return {
+
+	//tell 11ty that markdown, data and HTML files are processed by Nunjucks.js
+	markdownTemplateEngine: 'njk',
+	dataTemplateEngine: 'njk',
+	htmlTemplateEngine: 'njk',
+
+	//tel 11ty to look in the src folder for content, templates and other source code and
+	//use dist as output folder
+	dir: {
+	    input: 'src',
+	    output: 'dist'
+	}
+    };
 };
